306031eb4757c474b61d0d39f13dc19e187bae0a
cshinaver <bshinaver@gmail.com>
2016-02-04 02:08:18 -0500

    Added Topic model and migration

--- voting/models.py
+++ voting/models.py
@@ -2,4 +2,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+
+class Topic(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=200)
+
