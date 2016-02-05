cd1fbfb59224d2bc10e6f80a3a8399a9ecde0b05
cshinaver <bshinaver@gmail.com>
2016-02-05 00:47:01 -0500

    Added registration default view

--- ndlug/urls.py
+++ ndlug/urls.py
@@ -20,4 +20,5 @@ from django.conf.urls import include
 urlpatterns = [
     url(r'^$', include('announcements.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^registration/', include('registration.urls')),
 ]
--- registration/views.py
+++ registration/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
-# Create your views here.
+
+def index(request):
+    return render_to_response(
+        'registration/index.html',
+        context_instance=RequestContext(request),
+    )
