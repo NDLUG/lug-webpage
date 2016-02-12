a2ea22a1bac3ee12ace020eca48ab70175f5cb90
cshinaver <bshinaver@gmail.com>
2016-02-12 03:46:28 -0500

    Added django-secretballot (required migration)

--- ndlug/settings.py
+++ ndlug/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django_nose',
     'core',
     'voting',
+    'secretballot',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -51,6 +52,7 @@ MIDDLEWARE_CLASSES = [
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'secretballot.middleware.SecretBallotIpMiddleware',
 ]
 
 ROOT_URLCONF = 'ndlug.urls'
